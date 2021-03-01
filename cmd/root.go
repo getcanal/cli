21f7bfe6522edb1739bb92ce929b9f61cb401adc
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2021-03-01 14:15:08 +0800

    feat: use viper for configuration

--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,11 @@
 package cmd
 
 import (
+	"cli/util"
 	"fmt"
 	"github.com/spf13/cobra"
 	"os"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
 )
 
@@ -13,15 +13,9 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
+	Use:   "canal",
+	Short: "A Command-line interface of Canal platform",
+	// Uncomment the following line if bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
 }
@@ -41,36 +35,42 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cli.yaml)")
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.canal/config.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
 		// Use config file from the flag.
+		// Not supported now.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
+		if !util.CanalConfigExists() {
+			err := util.InitializeCanalConfig()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+
+		canalDirPath, err := util.CanalDirPath()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		// Search config in home directory with name ".cli" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".cli")
+		viper.AddConfigPath(canalDirPath)
+		viper.SetConfigName(util.ConfigFileName)
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+		// fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
 }
--- util/store.go
+++ util/store.go
@@ -2,27 +2,16 @@ package util
 
 import (
 	"cli/api/auth"
-	"io/ioutil"
+	"fmt"
+	"github.com/spf13/viper"
 	"os"
 )
 
+const ConfigFileName = "config"
+
 func StoreProjectToken(token auth.ProjectToken, project string) error {
-	err := assertCanalDirExists()
-	if err != nil {
-		return err
-	}
-
-	path, err := projectTokenFilePath(project)
-	if err != nil {
-		return err
-	}
-
-	file, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-
-	_, err = file.WriteString(string(token))
+	viper.Set(project+"_project_token", token)
+	err := viper.WriteConfig()
 	if err != nil {
 		return err
 	}
@@ -31,36 +20,13 @@ func StoreProjectToken(token auth.ProjectToken, project string) error {
 }
 
 func GetProjectToken(project string) (auth.ProjectToken, error) {
-	path, err := projectTokenFilePath(project)
-	if err != nil {
-		return auth.ProjectToken(""), err
-	}
-
-	token, err := ioutil.ReadFile(path)
-	if err != nil {
-		return auth.ProjectToken(""), err
-	}
-
+	token := viper.GetString(project + "_project_token")
 	return auth.ProjectToken(token), nil
 }
 
 func StoreUserToken(token auth.UserToken) error {
-	err := assertCanalDirExists()
-	if err != nil {
-		return err
-	}
-
-	path, err := userTokenFilePath()
-	if err != nil {
-		return err
-	}
-
-	file, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-
-	_, err = file.WriteString(string(token))
+	viper.Set("user_token", token)
+	err := viper.WriteConfig()
 	if err != nil {
 		return err
 	}
@@ -69,38 +35,33 @@ func StoreUserToken(token auth.UserToken) error {
 }
 
 func GetUserToken() (auth.UserToken, error) {
-	path, err := userTokenFilePath()
-	if err != nil {
-		return auth.UserToken(""), err
-	}
-
-	token, err := ioutil.ReadFile(path)
-	if err != nil {
-		return auth.UserToken(""), err
-	}
-
+	token := viper.GetString("user_token")
 	return auth.UserToken(token), nil
 }
 
-func userTokenFilePath() (string, error) {
-	canal, err := canalDirPath()
+func InitializeCanalConfig() error {
+	path, err := CanalConfigPath()
+	if err != nil {
+		return err
+	}
+
+	_, err = os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func CanalConfigPath() (string, error) {
+	path, err := CanalDirPath()
 	if err != nil {
 		return "", err
 	}
-
-	return canal + string(os.PathSeparator) + "user_token", nil
+	return path + string(os.PathSeparator) + ConfigFileName + ".yml", nil
 }
 
-func projectTokenFilePath(project string) (string, error) {
-	canal, err := canalDirPath()
-	if err != nil {
-		return "", err
-	}
-
-	return canal + string(os.PathSeparator) + project + "_project_token", nil
-}
-
-func canalDirPath() (string, error) {
+func CanalDirPath() (string, error) {
 	dir, err := os.UserHomeDir()
 	if err != nil {
 		return "", err
@@ -111,17 +72,18 @@ func canalDirPath() (string, error) {
 	return path, nil
 }
 
-func assertCanalDirExists() error {
-	path, err := canalDirPath()
+func CanalConfigExists() bool {
+	path, err := CanalConfigPath()
 	if err != nil {
-		return err
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		err := os.Mkdir(path, os.ModePerm)
-		if err != nil {
-			return err
-		}
+	if _, err := os.Stat(path); err == nil {
+		return true
+	} else if os.IsNotExist(err) {
+		return false
+	} else {
+		return false
 	}
-	return nil
 }
